c0f3b2df0116f124b8d799805d865ebd9a08106e
sivakumar <ksiva7393@gmail.com>
2026-05-09 19:26:05 +0530

    added cors config

--- src/main/java/com/goodmorning/config/SecurityConfig.java
+++ src/main/java/com/goodmorning/config/SecurityConfig.java
@@ -2,21 +2,25 @@ package com.goodmorning.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)
-            throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            )
+            .formLogin(form -> form.disable());
 
         return http.build();
     }
